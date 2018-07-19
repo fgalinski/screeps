f00d37514b8fb9afb5355269be1423f64af3ebe7
Fabian Galinski <fabian.galinski@gmail.com>
2018-07-19 23:20:19 +0200

    Update

--- main.js
+++ main.js
@@ -4,4 +4,6 @@ let roleGeneral = require('role-general');
 module.exports.loop = function () {
     spawnGeneral.handleSpawning();
     roleGeneral.handleRoles();
+
+    // Building findPathTo vom Spawn zu den sourcen, dann straße bauen.
 };
