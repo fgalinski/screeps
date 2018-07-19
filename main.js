6d1db7249a8d360c34229eeaa9928c15a2696057
Fabian Galinski <fabian.galinski@gmail.com>
2018-07-19 23:23:59 +0200

    Update

--- main.js
+++ main.js
@@ -5,5 +5,8 @@ module.exports.loop = function () {
     spawnGeneral.handleSpawning();
     roleGeneral.handleRoles();
 
+    // Constructor und Wartung
     // Building findPathTo vom Spawn zu den sourcen, dann straße bauen.
+    // Dann Extennsions
+    // Room COntroller muss auf lvl 2 sein.
 };
